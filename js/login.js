bb2e0958f2a58adf0d2cfc0793636fbca4d4940e
mi usuario de github <alejobaudy@gmail.com>
2024-11-18 18:57:50 -0300

    Se puso el header y se añadió un formulario para el login, ademas se modificó apenas el javascript del login.

---
+++ js/login.js
@@ -0,0 +1,5 @@
+let logiin = document.querySelector(".mail");
+let contra = document.querySelector(".contra"); 
+let mensajito = document.querySelector(".mensajemail")
+let mensajitoo = document.querySelector(".mensajecontra")
+
