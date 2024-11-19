d89b1ff929158c9c97657454ce79c576c4772131
mi usuario de github <alejobaudy@gmail.com>
2024-11-19 16:45:03 -0300

    Intento de agregar el contenido del fetch, para plasmarlo en pantalla.

--- js/home.js
+++ js/home.js
@@ -9,30 +9,52 @@ formuhome.addEventListener("submit",function(event){
         resphomee.innerText = "Tenes que poner algo.";
         event.preventDefault()
         return;
-    }else if(searchinput.value < 3){
+    }else if(searchinput.value <= 3){
         home.style.display = "block";
         resphomee.innerText = "Tiene que tener más de tres letras la busqueda.";
         event.preventDefault()
         return;
-    }else if(searchinput === "categorias"){
+    }else if(searchinput == "categorias"){
         window.location.href = "categorias.html";
         return;
-    }else if(searchinput === "detalle"){
+    }else if(searchinput == "detalle"){
         window.location.href = "detalle.html";
         return;
     }else if(searchinput === "hometalle"){
         window.location.href = "home.html";
         return;
-    }else if(searchinput === "registro"){
+    }else if(searchinput == "registro"){
         window.location.href = "registro.html";
         return;
-    }else if(searchinput === "login"){
+    }else if(searchinput == "login"){
         window.location.href = "login.html";
         return;
-    }else if(searchinput === "categoria"){
+    }else if(searchinput == "categoria"){
         window.location.href = "categoria.html";
         return;
-    }else{
-        resphomee.innerText = "No se encontró su busqueda."
     }
-})
+})  
+fetch('https://dummyjson.com/recipes')
+    .then(function (response){
+        return response.json();
+    })
+    
+    .then(function (data){    
+        for (let i=0; i < 10; i++){
+            let comida = data.recipes[i];
+            
+            let contenido = 
+                `<div class = "contenidorec">
+                    <img src = ${comida.image}/>
+                    <p class = "nombre"> ${comida.name} </p>
+                    <P class = "dificultad"> Nivel de dificultad: ${comida.difficulty} </p>
+                    <p class = "info"> <a href=comida.html?id=${comida.id} Ver más </a></p>
+                `;
+            comida += contenido;
+        }
+        containerrecetas.innerHTML = comida;
+
+    })
+    .catch(function (error){
+        console.log("Mi error fue", error);
+    })
