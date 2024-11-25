e07c71c18e3ef469e5c7dbc3dbf66b9b00b37b4d
Ignacio Sojo <ignaciosojo@MacBook-Air-de-Ignacio.local>
2024-11-24 21:06:26 -0300

    chequeo

---
+++ js/detalle.js
@@ -0,0 +1,43 @@
+let queryString = location.search
+let queryStringObj = new URLSearchParams(queryString);
+let id = queryStringObj.get("id")
+
+let nombreDetalle= document.querySelector(".nombre_detalle")
+let imagenDetalle = document.querySelector(".imagen_detalle")
+let instruccionesDetalle = document.querySelector(".instrucciones_detalle")
+let tiempo_coccionDetalle = document.querySelector(".tiempo_detalle")
+let categoriasDetalle = document.querySelector(".categorias_detalle")
+
+let detalleIndividual={
+    name: "",
+    image:"",
+    instructions: "",
+    cookTimeMinutes: "",
+    mealType: "",
+}
+fetch(`https://dummyjson.com/recipes/${id}`)
+.then(function(response) {
+    return response.json()
+  })
+  .then(function(data){
+   
+    
+    detalleIndividual.name = data.name
+    nombreDetalle.innerText = detalleIndividual.name
+
+    detalleIndividual.image = data.image
+    imagenDetalle.innerHTML = `<img src=${detalleIndividual.image} />`
+
+    detalleIndividual.instructions = data.instructions
+    instruccionesDetalle.innerText = detalleIndividual.instructions
+
+    detalleIndividual.cookTimeMinutes = data.cookTimeMinutes
+    tiempo_coccionDetalle.innerText = detalleIndividual.cookTimeMinutes
+
+    detalleIndividual.mealType = data.mealType
+    categoriasDetalle.innerText = detalleIndividual.mealType
+
+  })
+.catch(function(e){
+    console.log(e)
+})
