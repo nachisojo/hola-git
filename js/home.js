fb94eaa9d9484cb095f8bb36db6d1680a5ee179f
mi usuario de github <alejobaudy@gmail.com>
2024-11-29 12:35:09 -0300

    Home modificado, el boton de añadir mas.

--- js/home.js
+++ js/home.js
@@ -5,32 +5,6 @@ let searchinput = document.querySelector(".searchinput")
 
 let containerrecetas = document.querySelector(".containerrecetas");
  
-fetch('https://dummyjson.com/recipes')
-    .then(function (response){
-        return response.json();
-    })
-    
-    .then(function (data){   
-        let contenidos = '';
-        for (let i=0; i < 10; i++){
-            let comida = data.recipes[i];
-            
-            let contenido =
-                `<article class="contenidorec">
-                    <img src = ${comida.image}>
-                    <p class = "nombre"> ${comida.name} </p>
-                    <P class = "dificultad"> Nivel de dificultad: ${comida.difficulty} </p>
-                    <p class = "info"> <a class="botondetalle" href="detalle.html?id=${comida.id}"> Ver más </a></p>
-                </article>`;
-            contenidos += contenido
-        }
-        containerrecetas.innerHTML = contenidos;
-
-    })
-    .catch(function (error){
-        console.log("Mi error fue", error);
-    })
-
     let search2 = document.getElementById("buscador")
     let home = document.querySelector(".home")
     formuhome.addEventListener('submit', function(e){
@@ -55,3 +29,40 @@ fetch('https://dummyjson.com/recipes')
         this.submit()
         }
     })
+const containermas = document.querySelector('.containertres')
+
+const boton = document.querySelector('.get_more')
+
+let skip = 0
+
+function getData(){
+  fetch(`https://dummyjson.com/recipes?limit=10&skip=${skip}`)
+  .then((response)=>{
+    return response.json()
+  })
+  .then(function(data){
+    let contenidos = ""
+    for (let i=0; i < data.recipes.length; i++){
+        let comida = data.recipes[i];
+        
+        let contenido =
+            `<article class="contenidorec">
+                <img src = ${comida.image}>
+                <p class = "nombre"> ${comida.name} </p>
+                <P class = "dificultad"> Nivel de dificultad: ${comida.difficulty} </p>
+                <p class = "info"> <a class="botondetalle" href="detalle.html?id=${comida.id}"> Ver más </a></p>
+            </article>`;
+        contenidos += contenido
+    }
+    containerrecetas.innerHTML += contenidos;
+  })
+}
+
+getData(); 
+
+
+boton.addEventListener('click', function(e){
+e.preventDefault()
+  skip += 10
+  getData() 
+})
