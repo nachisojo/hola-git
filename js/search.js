ea36acb9b2df070a89178ba3967ec2e622bd7ca3
mi usuario de github <alejobaudy@gmail.com>
2024-11-29 11:16:39 -0300

    search casi hecho

--- js/search.js
+++ js/search.js
@@ -1,7 +1,24 @@
 let queryString = location.search;
 let queryStringObj = new URLSearchParams(queryString);
-let tagg = queryStringObj.get("id");
+let query = queryStringObj.get("q");
+let searchh = document.querySelector(".searchfinal")
 
-fetch('https://dummyjson.com/recipes/search?q=Margherita')
+fetch(`https://dummyjson.com/recipes/search?q=${query}`)
 .then(res => res.json())
-.then());
+.then(function(data){
+    let seearch = '';
+    for (let i=0; i < data.recipes.length ; i++){
+        let seaarch = data.recipes[i];
+        
+        let ressearch =
+            `<article class="contenidorec">
+                <img src = ${seaarch.image}>
+                <p class = "nombre"> ${seaarch.name} </p>
+                <P class = "dificultad"> Nivel de dificultad: ${seaarch.difficulty} </p>
+                <p class = "info"> <a class="botondetalle" href="detalle.html?id=${seaarch.id}"> Ver más </a></p>
+            </article>`;
+        seearch += ressearch
+    }
+    searchh.innerHTML = seearch;
+
+});
